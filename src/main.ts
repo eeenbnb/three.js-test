2e42a78f74c86e2a7377053aa6887a76493417fb
eeenbnb <eee.kz.www@gmail.com>
2017-09-27 11:51:33 +0900

    add class files

--- src/main.ts
+++ src/main.ts
@@ -1,41 +1,8 @@
 import * as THREE from 'three';
+import { Setup } from './setup';
 declare function require(x: string): any;
 (window as any).THREE = THREE;
 require('../node_modules/three/examples/js/controls/PointerLockControls');
-
-class Setup{
-  renderer:THREE.WebGLRenderer;
-  scene:THREE.Scene;
-  camera:THREE.PerspectiveCamera;
-  constructor(){
-    //renderer
-    this.renderer = new THREE.WebGLRenderer();
-    this.renderer.setSize( window.innerWidth, window.innerHeight );
-    //scene
-    this.scene = new THREE.Scene();
-    this.scene.background = new THREE.Color( 0xf0f0f0 );
-    this.scene.add( new THREE.GridHelper( 1000, 1000 ) );
-    this.scene.add( new THREE.AxisHelper(20) );
-    //camera
-    this.camera = new THREE.PerspectiveCamera(45, 800 / 600, 1, 100000);
-    this.camera.position.set(0, 1.7, -1.5);
-    this.camera.lookAt(new THREE.Vector3(0,1.7,0));
-    this.scene.add(this.camera);
-    //light
-    this.scene.add( new THREE.AmbientLight( 0xF0F0F0 ) );
-
-
-  }
-  getRenderer():THREE.WebGLRenderer{
-    return this.renderer;
-  }
-  getScene():THREE.Scene{
-    return this.scene;
-  }
-  getCamera():THREE.PerspectiveCamera{
-    return this.camera;
-  }
-}
 
 window.addEventListener('DOMContentLoaded', () => {
 
